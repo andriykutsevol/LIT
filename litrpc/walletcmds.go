32dbf993ac343daac00d16fedc73d3e02f7140a1
infografx <infograf.contact@gmail.com>
2019-09-30 14:14:12 +0300

    fix.

--- litrpc/walletcmds.go
+++ litrpc/walletcmds.go
@@ -138,8 +138,6 @@ type SendArgs struct {
 func (r *LitRPC) Send(args SendArgs, reply *TxidsReply) error {
 	var err error
 
-	fmt.Printf("SendArgs: %+v \n", args)
-
 	nOutputs := len(args.DestAddrs)
 	if nOutputs < 1 {
 		return fmt.Errorf("No destination address specified")
